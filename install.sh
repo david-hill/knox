0695738f1ea40235f212204629475b9782b1ea5b
root <root@knox.orion>
2018-08-05 10:16:01 -0500

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo dnf upgrade --refresh
+sudo dnf install dnf-plugin-system-upgrade
+
+
+
+
+sudo dnf system-upgrade download --refresh --releasever=rawhide
+sudo dnf system-upgrade reboot
+yum install terminator vim
