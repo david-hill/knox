7c1b12031ef1b2d06a3c636ae7404d8eddf001a0
root <root@knox.orion>
2018-08-05 18:15:43 -0500

    Add etc/yum.repos.d/*, copy them to final destination and install a sh*t load of packages.

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 sudo dnf upgrade --refresh
 sudo dnf install dnf-plugin-system-upgrade
+sudo cp etc/yum.repos.d/* /etc/yum.repos.d
 
-
-
-
-sudo dnf system-upgrade download --refresh --releasever=rawhide
-sudo dnf system-upgrade reboot
-yum install terminator vim
+if ! $(grep -qi rawhide /etc/redhat-release) ; then
+    sudo dnf system-upgrade download --refresh --releasever=rawhide --nogpgcheck
+    cd /etc/pki/rpm-gpg/
+    ls  | sort -k1,1V | grep x86_64 | tail -1  | xargs -I% sudo ln -s % RPM-GPG-KEY-fedora-rawhide-x86_64
+    sudo dnf system-upgrade reboot # https://bugzilla.redhat.com/show_bug.cgi?id=1612547
+else
+    sudo systemctl enable fstrim.service
+    sudo systemctl start fstrim.service
+    sudo yum install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
+    sudo yum install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
+    sudo yum install -y terminator vim google-chrome slack skypeforlinux vlc
+fi
